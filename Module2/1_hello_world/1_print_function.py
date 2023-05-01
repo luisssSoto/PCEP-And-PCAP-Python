23c5241a4064eb3af7d455f3fa902e04ec6de8e7
luisSoto <alejandrosotoserafin@gmail.com>
2023-04-30 20:45:45 -0600

    helloWorld

---
+++ Module2/1_hello_world/1_print_function.py
@@ -0,0 +1,56 @@
+'''The print() function'''
+
+#Double quotes
+print("Hello snakes" )
+
+#Simple quotes
+print('Hello World from Python')
+
+#print() works in only one row,so that, the code above is wrong
+#print() print()
+
+#Positional Arguments
+positionA="I'm in the position A"
+print(positionA, "I'm in the position B")
+
+#Keyword Arguments: "sep y end"
+print("my name is", " luis", " soto", sep="sss")
+
+print("Run faster,", "wait!", "Stop! there is a wal..", end="|")
+print()
+
+#You can join both of them
+print("Programming","Fundamentals","in", sep="***", end=" ")
+print("Python")
+
+#Remember the next sign \ means an interruption
+#New Line \n
+print("Each\n", "piece of code\n", "on one line")
+
+#Tabulator space  \t
+print('This is a', 'tabulator','space', sep='\t')
+
+#Visible quotes
+print("Did you watch the new movie \"Mario Bros\"?")
+print('Did you see the new movie "Mario Bros"?')
+
+# Every form you want
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#Multiline print
+print('''Hi
+I'm a multiline
+it's awesome
+isn't it?''')
+
+
+
+
+
