ec30c9bb23d6a7e9253a5e9a5a0db7fa6b6c85dc
luisSoto <alejandrosotoserafin@gmail.com>
2023-05-05 07:55:45 -0600

    operators and operands

---
+++ Module2/3_operators/6_module.py
@@ -0,0 +1,5 @@
+'''Module'''
+print("module:", 10%3)
+
+operand1,operand2=15,7
+print(operand1, '%', operand2, '=', operand1%operand2)
---
+++ Module2/3_operators/9_hierarchy.py
@@ -0,0 +1,7 @@
+''' Operator's Hierarchy and then left link'''
+
+print("This is hierarchy:", 1 - 3 *2 **2)
+print("This is left link:", 10/2/5)
+print("but there is an exception... with the exponent:")
+print(2**3**2, 'the last result was because we used a rigth link')
+print("Remember all inside parentheses have greatest hierarchy:", (5 * ((25 % 13) + 100) / (2 * 13)) // 2)
