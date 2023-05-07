50d15be5f76e127d823c83961471e0c541ec6bfd
luisSoto <alejandrosotoserafin@gmail.com>
2023-05-06 20:32:17 -0600

    comments

---
+++ Module2/5_comments/1.comments.py
@@ -0,0 +1,16 @@
+"""Comments"""
+print("Welcome to comments")
+#This is a comment
+
+#This is
+#a comment
+#in multiply lines
+
+#Note: Remember that It's more important to let the program speaks by itself than a lot of comments
+#You can get the last one with auto-describing variables and functions:
+
+#This variable is better to storage the value number 1
+value1 = 1
+
+#And this one is more abstract
+paranguakutirimicuaro = 1
