130612ea101bf6156a8e039d17407ac0dde7a42a
Luis Soto <alejandrosotoserafin@gmail.com>
2024-04-10 22:40:32 -0600

    testModule1.3

---
+++ PCAP/Module1/1.4_test/mainDirectory/main.py
@@ -0,0 +1,7 @@
+import sys 
+
+sys.path.append("C:\\Programming Languages\\Python\\PCEPPython\\PCEP-Certified-Entry-Level-Python-Programmer-\\PCAP\\Module1\\1.4_test\\moduleDirectory\\Anime")
+
+import kodomo.doraemon
+
+print(kodomo.doraemon.funDoraemon())
---
+++ PCAP/Module1/1.4_test/moduleDirectory/Anime/kodomo/doraemon.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import sys
+
+"""module: Doraemon"""
+
+def funDoraemon():
+    return 'module Doraemon'
+
+if __name__ == "__main__":
+    print("Don't execute it from here!")
+    sys.exit()
+else:
+    print("Welcome to Doraemon module")
