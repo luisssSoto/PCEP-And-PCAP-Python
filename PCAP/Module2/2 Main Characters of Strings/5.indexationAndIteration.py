5e9083a573f4d851f698880f898ef1c726d699a7
Luis Soto <alejandrosotoserafin@gmail.com>
2024-04-19 18:01:14 -0600

    strings2

---
+++ PCAP/Module2/2 Main Characters of Strings/5.indexationAndIteration.py
@@ -0,0 +1,14 @@
+"""Indexation and Iteration in Strings"""
+'''Indexation'''
+string = 'Programmer'
+for i in range(len(string)):
+    print(string[i], end='')
+
+print()
+
+'''Iteration'''
+for character in string:
+    print(character, end='') 
+print()
+
+print(string[0])
---
+++ PCAP/Module2/2 Main Characters of Strings/7.inAndNotInInstructions.py
@@ -0,0 +1,11 @@
+""" in And not in functions"""
+
+''' in()'''
+alphabet = 'abcdefghijklmnñopqrstuvwxyz'
+print('abc' in alphabet)
+print('A' in alphabet)
+
+'''not in()'''
+print('lefg' not in alphabet)
+print('3' not in alphabet)
+print('yz' not in alphabet)
