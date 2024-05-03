240ba08f98991e9e3a563e33b9337dfbc25e30fe
Luis Soto <alejandrosotoserafin@gmail.com>
2024-05-03 08:46:38 -0600

    readingIntegersSafetely

--- PCAP/Module2/7_Common Exceptions/2.assertionError.py
+++ PCAP/Module2/7_Common Exceptions/2.assertionError.py
@@ -9,3 +9,5 @@ try:
     # asuring the string contains something inside of it
 except AssertionError as e:
     print('Assertion Error: ', e)
+    
+# Note: AssertionError will be launched when the values are: False, 0, None and an empty string
