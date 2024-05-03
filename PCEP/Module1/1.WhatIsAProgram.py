ab0a06d1e19c0bcea935cbc719845a32d7443ac5
Luis Alejandro Soto Serafin <Luis.Alejandro.Soto.Serafin@ibm.com>
2024-05-03 13:56:33 -0600

    translatIntoEnglish

---
+++ PCEP/Module1/1.WhatIsAProgram.py
@@ -0,0 +1,11 @@
+'''Computer programs nowadays are so common that we use them daily,
+often without even being aware of it. We use them to listen to a 
+song, to find the best route to our destination, to search a 
+website, use social networks, instant messaging, and the list goes 
+on. Every day, hundreds of applications are launched, generally 
+helping to simplify our daily lives.
+
+All these applications or programs are written using programming, 
+which is nothing more than writing a list of instructions (IL) for 
+our computer to execute and then run that famous application you 
+use daily.'''
---
+++ PCEP/Module1/2.Languages.py
@@ -0,0 +1,15 @@
+'''There are two types of languages:
+Natural language, such as our mother tongue or body language.
+Machine language, which is what our computer reads and understands, 
+often depicted as a series of zeros and ones grouped together 
+(01010) in science fiction movies.
+
+Regardless of the language, they must have certain characteristics:
+Alphabet
+Lexicon
+Syntax
+Semantics
+
+To communicate with our computer and make it execute an IL, we need
+a language that acts as a bridge between our mother tongue and 
+machine language, namely a high-level programming language.'''
