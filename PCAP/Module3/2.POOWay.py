d9e365518334cce01b21dc0de89357f7b3df1a6a
Luis Soto <alejandrosotoserafin@gmail.com>
2024-05-03 17:56:02 -0600

    module3

---
+++ PCAP/Module3/2.POOWay.py
@@ -0,0 +1,23 @@
+""" POO Way """
+'''POO'''
+''' it is a way to take any idea of the real world and convert it to a 
+class, so in this scenario the data lives, let me explain with an example: '''
+
+'''Class'''
+# Anime 
+
+'''but at the same time, it could be more classes that belong the first one:'''
+
+''' Superclass '''
+# Anime
+
+'''Subclass'''
+# Kodomo       
+'''Subclass'''
+# Shonen
+
+'''So, the hierarchy could be just like this, as you can see exist one
+superclass (Anime) and two subclases (Kodomo and Shonen) which belong
+to Anime '''
+# Anime <- Kodomo 
+#       <- Shonen
