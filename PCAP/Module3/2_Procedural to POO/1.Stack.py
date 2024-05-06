f79b6e73b4e6b390ce54045183e3dffd33f02e66
Luis Soto <alejandrosotoserafin@gmail.com>
2024-05-06 10:19:53 -0600

    secondPartPOO

---
+++ PCAP/Module3/2_Procedural to POO/1.Stack.py
@@ -0,0 +1,19 @@
+""" What is a Stack? """
+''' A stack is a like a place to save something where this place has a particular 
+behavior calls LIFO (Last In - First Out) which means that the last object which got in
+the stack is the first one is going to out, in nutshills there is only a 1 entrance but 
+at the same time, it is 1 exit as well'''
+
+''' There are two particular actions that the stack could have
+1. push... push something from top the stack
+2. pop... pop something from top the stack'''
+
+'''Stack:
+_____________
+|  object   | -----> top
+|    |      |
+|    |      |
+|    |      |
+|    |      |
+|    V      |
+|___________| -----> botom'''
---
+++ PCAP/Module3/2_Procedural to POO/3.disadvantages.py
@@ -0,0 +1,15 @@
+""" Disadvantages of Procedural Approach """
+'''
+1. The main variable; stack is not protected so anyone accidentally or not could damage
+it 
+2. The code could be growth excessively, when we want to repeat the number of the stacks
+or the same functions of the stacks
+3. Maybe we want to create more functions for the stack and don't do it from 0, because
+you've already write a couple of functions, so you want to start from there'''
+
+'''Advantages of POO
+It is interesting how the advantages of the POO handle with the disadvantages of the 
+Procedural Approach:
+1. Encapsulation
+2. Classes
+3. Inheritance'''
