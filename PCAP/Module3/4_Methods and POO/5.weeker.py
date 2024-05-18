a57fcc9ead8ab67f77761136758d08355f38bfce
Luis Soto <alejandrosotoserafin@gmail.com>
2024-05-17 22:18:40 -0600

    weeker

--- PCAP/Module3/4_Methods and POO/5.weeker.py
+++ PCAP/Module3/4_Methods and POO/5.weeker.py
@@ -5,48 +5,79 @@ class WeekDayError(Exception):
 
 class Weeker:
     __week = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom']
-    #
-    # Escribir código aquí.
-    #
+    __daysOfWeek = len(__week)
 
     def __init__(self, day):
         self.__day = day
         if self.__day not in Weeker.__week:
             raise WeekDayError
-
+ 
     def __str__(self):
-        pass
-        #
-        # Escribir código aquí.
-        #
+        return self.__day
 
     def add_days(self, n):
-        pass
-        #
-        # Escribir código aquí.
-        #
+        self.__position = Weeker.__week.index(self.__day)
+        self.__amount_days = n % Weeker.__daysOfWeek
+        self.__distance = 6 - self.__position
+        if self.__amount_days > self.__distance:
+            self.__new_position = self.__amount_days - self.__distance - 1
+            self.__day = Weeker.__week[self.__new_position]
+        else:
+            self.__day = Weeker.__week[self.__position + self.__amount_days]
 
-    def subtract_days(self, n):
-        pass
-        #
-        # Escribir código aquí.
-        #
+    def substract_days(self, n):
+        self.__position = Weeker.__week.index(self.__day)
+        self.__amount_days = n % self.__daysOfWeek
+        self.__distance = self.__position
+        if self.__amount_days > self.__distance:
+            self.__new_position = self.__amount_days - self.__distance
+            self.__day = Weeker.__week[-self.__new_position]
+        else:
+            self.__day = Weeker.__week[self.__amount_days - 1]
 
 try:
-    day = Weeker('Lu')
+    weekday = Weeker('Lun')
+    print(weekday)
+    weekday.add_days(3)
+    print(weekday)
+    weekday.substract_days(12)
+    print(weekday)
+    weekday = Weeker('Lunes')
 except WeekDayError:
-    print(WeekDayError.adivice)
-    print('continue')
+    print("Lo siento, no puedo atender tu solicitud.")
+print()
+
+''' Best solution '''
+class WeekDayError(Exception):
+    pass
 
 
-# try:
-#     weekday = Weeker('Lun')
-#     print(weekday)
-#     weekday.add_days(15)
-#     print(weekday)
-#     weekday.subtract_days(23)
-#     print(weekday)
-#     weekday = Weeker('Lunes')
-# except WeekDayError:
-#     print("Lo siento, no puedo atender tu solicitud.")
-    
+class Weeker:
+    __names = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom']
+
+    def __init__(self, day):
+        try:
+            self.__current = Weeker.__names.index(day)
+        except ValueError:
+            raise WeekDayError
+
+    def __str__(self):
+        return Weeker.__names[self.__current]
+
+    def add_days(self, n):
+        self.__current = (self.__current + n) % 7
+
+    def subtract_days(self, n):
+        self.__current = (self.__current - n) % 7
+
+
+try:
+    weekday = Weeker('Lun')
+    print(weekday)
+    weekday.add_days(3)
+    print(weekday)
+    weekday.subtract_days(12)
+    print(weekday)
+    weekday = Weeker('Lunes')
+except WeekDayError:
+    print("Lo siento, no puedo atender tu solicitud.")
