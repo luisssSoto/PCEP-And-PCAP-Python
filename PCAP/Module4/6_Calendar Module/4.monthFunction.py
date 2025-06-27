90b4a214aaee97a77c1b3cf5b59a24593e5d6b36
AlexSoto <alejandrosotoserafin@gmail.com>
2025-06-26 21:50:03 -0500

    add month and prmonth functions

---
+++ PCAP/Module4/6_Calendar Module/4.monthFunction.py
@@ -0,0 +1,8 @@
+"""month function"""
+import calendar
+print(calendar.month(2020, 2))
+
+# additionals parameters
+# w -> space between colomuns
+# l -> space between rows
+print(calendar.month(2020, 2, 2, 3))
